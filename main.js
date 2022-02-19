0440784c2c5fd2d7a24884c65d3b946ef20aaff5
Yiran <28963366+ychen131@users.noreply.github.com>
2022-02-19 13:44:30 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+// create age variable
+const myAge = 6;
+
+// create variable earlyYears 
+var earlyYears = 2;
+
+// reassign the variable earlyYears
+earlyYears *= 10.5;
+
+// create variable laterYears
+var laterYears = myAge - 2;
+
+// reassign the variable laterYears
+laterYears *= 4;
+
+// calculate my age in dog years by combining earlyYears and later years
+var myAgeInDogYears = earlyYears + laterYears;
+
+// create variable for my name
+var myName = 'Pocky';
+
+// update my name to all lower cases
+myName = myName.toLowerCase();
+
+// log my name and age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years 
+which is ${myAgeInDogYears} years old in dog years.`);
+
